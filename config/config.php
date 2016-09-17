b0f6e4f39551b988f1790d03f8d622b41e163581
Daniel Choi <cydp0127@gmail.com>
2016-09-17 16:27:58 +0900

    add config
    add config

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+/* WINDOWS TEST CONFIG */
+
+define("DBSERVERNAME", "your dbservername");     // The host you want to connect to.
+define("DBUSERNAME", "your username");    // The database username.
+define("DBPASSWORD", "your password");    // The database password.
+define("DBNAME", "your dbname");    // The database name.
+define("URL","your url");

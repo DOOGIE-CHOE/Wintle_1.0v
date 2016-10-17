787a0340c9eda50629948ab7c6007f401abb02e8
Daniel Choi <cydp0127@gmail.com>
2016-10-17 18:07:05 +0900

    Mypage Project View
    Mypage Project View

---
+++ application/views/mypage/projects.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 10/17/2016
+ * Time: 6:06 PM
+ */
+
+class Projects extends View{
+    function __construct() {
+        parent::__construct();
+    }
+}

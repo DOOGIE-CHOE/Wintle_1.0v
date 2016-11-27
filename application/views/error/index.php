6a3e9c8ee16aace0b469f08f187d60b7d01f68c4
Daniel Choi <cydp0127@gmail.com>
2016-11-27 18:16:44 +0900

    add background for error page
    add background for error page

--- application/views/error/index.php
+++ application/views/error/index.php
@@ -6,9 +6,29 @@
  * Time: 5:07 PM
  */
 ?>
+<head>
+    <style>
+        #bg{
+            position:fixed;
+            height:100%;
+            width:100%;
+            background-repeat: no-repeat;
+            background-image: url("<?php echo URL?>img/bg/bg1.png");
+            background-size: cover;
+        }
+    </style>
+</head>
 
-<div id="all">
-    <body id="body">
-        <?php echo $this->msg;?>
+    <body id="body">add error handling on shared
+
+    <div id="bg"></div>
+    <div>
+
+
+
+    </div>
+    <?php echo $this->msg;?>
     </body>
 </div>
+
+<div id="all">

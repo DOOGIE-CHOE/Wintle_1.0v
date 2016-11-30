79a2a62a2b5112ab635cfcd836819cfa21d8742c
Daniel Choi <cydp0127@gmail.com>
2016-11-30 19:06:11 +0900

    add Message skeleton
    add Message skeleton

---
+++ application/controllers/message.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 11/30/2016
+ * Time: 7:03 PM
+ */
+
+
+class Message extends Controller {
+    function __construct() {
+        parent::__construct();
+    }
+}
---
+++ application/models/message_model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 11/30/2016
+ * Time: 7:04 PM
+ */
+class LogIn_Model extends Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+
+}

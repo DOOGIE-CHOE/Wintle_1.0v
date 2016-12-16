01b27d2007a363f257846b196989b676d719ff90
Daniel Choi <cydp0127@gmail.com>
2016-12-16 19:06:23 +0900

    modified content table
    modified content table

--- db/sql/create_table_content.sql
+++ db/sql/create_table_content.sql
@@ -1,8 +1,9 @@
 create table content (
 	content_id int primary key,
     content_title varchar(255) not null,
-    comments varchar(1000),
+    content_path varchar(255) not null,
+    comments varchar(255),
     upload_date  timestamp default current_timestamp,
     modified_date timestamp  default 0,
-    view_count int
+    view_count int default 0
 );

dad53dfc65fbac9bb668abee775eb8612fde9cfa
Daniel Choi <cydp0127@gmail.com>
2016-12-26 15:00:30 +0900

    view table backup
    view table backup

--- db/sql/create_view_content_with_hashtag.sql
+++ db/sql/create_view_content_with_hashtag.sql
@@ -2,15 +2,18 @@ CREATE
     ALGORITHM = UNDEFINED 
     DEFINER = `root`@`localhost` 
     SQL SECURITY DEFINER
-VIEW `view_content_hashtag` AS
+VIEW `view_content_with_hashtag` AS
     SELECT 
-        `hlist`.`content_id` AS `content_id`,
-        `hlist`.`hashtag_id` AS `hashtag_id`,
-        GROUP_CONCAT(`htag`.`tag_name`
-            SEPARATOR ',') AS `hashtags`
+        `con`.`user_id` AS `user_id`,
+        `con`.`content_id` AS `content_id`,
+        `con`.`content_type_name` AS `content_type_name`,
+        `con`.`content_title` AS `content_title`,
+        `con`.`content_path` AS `content_path`,
+        `con`.`comments` AS `comments`,
+        `con`.`upload_date` AS `upload_date`,
+        `con`.`modified_date` AS `modified_date`,
+        `con`.`view_count` AS `view_count`,
+        `hashs`.`hashtags` AS `hashtags`
     FROM
-        (`hashtag_list` `hlist`
-        JOIN `hashtag` `htag`)
-    WHERE
-        (`hlist`.`hashtag_id` = `htag`.`hashtag_id`)
-    GROUP BY `hlist`.`content_id`
+        (`view_content_info` `con`
+        LEFT JOIN `view_content_hashtag` `hashs` ON ((`con`.`content_id` = `hashs`.`content_id`)))

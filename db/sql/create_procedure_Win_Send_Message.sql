f5830816d810a987c046780cff007c57c99341ed
Daniel Choi <cydp0127@gmail.com>
2017-01-05 15:44:37 +0900

    modified send message precedure
    modified send message precedure

--- db/sql/create_procedure_Win_Send_Message.sql
+++ db/sql/create_procedure_Win_Send_Message.sql
@@ -7,47 +7,47 @@ in _message varchar(1000),
 out _return int)
 this : BEGIN
 
-    declare _opponent int default 0;
-    declare _message_group_id int default 0;
-	declare _group_sequence int default null;
-	declare _result int default 0;
+    declare opponent int default 0;
+    declare message_group int default 0;
+	declare group_sequence int default null;
+	declare result int default 0;
     
     
-    /*declare EXIT handler for sqlexception, sqlwarning
+    declare EXIT handler for sqlexception, sqlwarning
     begin
     rollback;
     set _return = -100;
     end;
-    */
-	start transaction;
     
+	start transaction;
+
+
 	/* check if user exists */
-	select count(user_id) into _result from user where user_id = _user_sender or user_id = _user_receiver;
-	if _result <> 2 then
+	select count(user_id) into result from user where user_id = _user_sender or user_id = _user_receiver;
+	if result <> 2 then
 		set _return = -1;
         leave this;
 	end if;
 	
     /* check if there is existing conversation */
-	select count(user_id), msg_group_id into _opponent, _message_group_id from message_list 
+	/*select count(user_id), msg_group_id into opponent, message_group from message_list 
     where msg_group_id 
     in(select msg_group_id from message_list where msg_group_id in (select msg_group_id from message_list where user_id = _user_sender)
 	group by msg_group_id
 	having count(user_id) = 2)
-	and user_id = _user_receiver;
+	and user_id = _user_receiver;*/
     
-	/*select count(user_id) user_count, msg_group_id into _opponent, _message_group_id from message_list where msg_group_id = (
-	select msg_group_id from message_list where user_id = _user_sender)
-	and user_id in (_user_receiver, _user_sender)
-	group by msg_group_id;
-    */
+    select count(m.msg_group_id), m.msg_group_id into opponent, message_group from message_list m
+	inner join message_list n
+	on m.msg_group_id = n.msg_group_id
+	where m.user_id = 100001002 and
+	n.user_id = 100001032;
     
-    if _opponent = 2 then
-			insert into user_message (user_id, msg_group_id, message) values(_user_sender, _message_group_id,_message);
+    if opponent = 1 then
+			insert into user_message (user_id, msg_group_id, message) values(_user_sender, message_group,_message);
             set _return = 1;
-	elseif _opponent = 1 then 
-			select GET_SEQUENCE('message') into _group_sequence;
-			insert into message_group (msg_group_id, msg_type) values (group_sequence, 1);
+	elseif opponent = 0 then
+			select GET_SEQUENCE('message') into group_sequence;
             insert into message_list (user_id, msg_group_id) values (_user_sender, group_sequence);
             insert into message_list (user_id, msg_group_id) values (_user_receiver, group_sequence);
 			insert into user_message (user_id, msg_group_id, message) values(_user_sender, group_sequence,_message);
@@ -57,7 +57,5 @@ this : BEGIN
         leave this;
     end if;
     
-    
     commit;
-    
 END

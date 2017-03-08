2a8377c6d887235312e62e945b47ade74fa42414
Daniel Choi <cydp0127@gmail.com>
2017-03-08 15:12:20 +0900

    #2013 server/local settings

--- library/bootstrap.php
+++ library/bootstrap.php
@@ -1,18 +1,18 @@
 <?php
-//
-//$protocol = "https://";
-//$path = null;
-//if (substr($_SERVER['HTTP_HOST'], 0, 4) !== 'www.') {
-//    $path = $protocol.'www.'.$_SERVER['HTTP_HOST'];
-//    header('Location: '.$path);
-//    exit;
-//}else if(substr($_SERVER['HTTP_HOST'], 0, 4) == 'www.' && ($_SERVER["HTTPS"] == "off" || !isset($_SERVER["HTTPS"]))){
-//    $path = $protocol.$_SERVER['HTTP_HOST'];
-//    header('Location: '.$path);
-//    exit;
-//}
 
-
+if($isServer){
+    $protocol = "https://";
+    $path = null;
+    if (substr($_SERVER['HTTP_HOST'], 0, 4) !== 'www.') {
+        $path = $protocol.'www.'.$_SERVER['HTTP_HOST'];
+        header('Location: '.$path);
+        exit;
+    }else if(substr($_SERVER['HTTP_HOST'], 0, 4) == 'www.' && ($_SERVER["HTTPS"] == "off" || !isset($_SERVER["HTTPS"]))){
+        $path = $protocol.$_SERVER['HTTP_HOST'];
+        header('Location: '.$path);
+        exit;
+    }
+}
 
 require_once (ROOT . DS . 'config' . DS . 'config.php');
 require_once (ROOT . DS . 'library' . DS . 'shared.php');
--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+//** IMPORTANT **/
+/*
+ * you should set isServer variable true if you want to use this file at server
+  */
+$isServer = false;
+
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 
